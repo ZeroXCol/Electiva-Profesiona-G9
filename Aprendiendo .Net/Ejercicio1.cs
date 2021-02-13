6f40a5285bb5f25e29a4a5189f5844927a1d1a9c
Fabian Martinez <xeroxcolombia15@gmail.com>
2021-02-13 09:01:18 -0500

    Agregar archivos de proyecto.

---
+++ Aprendiendo .Net/Ejercicio1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aprendiendo_.Net
+{
+    class Ejercicio1
+    {
+        public void Programas()
+        {
+            double Numero1, Numero2, Suma = 0, Resta = 0, Producto, Division = 0, Multiplicacion = 0;
+
+            string Entrada;
+            Console.WriteLine("Opcion uno Operaciones Matematicas");
+            Console.WriteLine("Selecciona la Operacion");
+            Console.WriteLine("+");
+            Console.WriteLine("-");
+            Console.WriteLine("*");
+            Console.WriteLine("/");
+
+            Entrada = Console.ReadLine();
+
+            Console.WriteLine("Digite el primer numero " );
+            Numero1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite el segundo numero ");
+            Numero2 = double.Parse(Console.ReadLine());
+
+
+            if (Entrada == "+")
+            {
+            
+
+                Suma = Numero1 + Numero2;
+                Console.WriteLine("Valor de la Suma es " + Suma);
+
+            }
+            if (Entrada == "-")
+            {
+                Console.WriteLine("Resta \n");
+
+                Resta = Numero1 - Numero2;
+                Console.WriteLine("Valor de la Resta es " + Resta);
+            }
+            if (Entrada == "/")
+            {
+
+                if (Numero2 != 0) {
+
+                    Division = Numero1 / Numero2;
+                    Console.WriteLine("Valor de la División es " + Division);
+                }
+                else
+                {
+                    Console.Write("El segundo numero debe ser diferente a 0");
+                    while (Numero2 == 0)
+                    {
+                        Console.WriteLine("Digite el segundo numero");
+                        Numero2 = double.Parse(Console.ReadLine());
+
+                    }
+
+                    Division = Numero1 / Numero2;
+                    Console.WriteLine("Valor de la División es " + Division);
+
+                }
+
+            }
+            if (Entrada == "*")
+            {
+
+                Multiplicacion = Numero1 * Numero2;
+                Console.WriteLine("Valor de la Multiplicación es " + Multiplicacion);
+
+            }
+
+            
+           
+           
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+}
---
+++ Aprendiendo .Net/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Aprendiendo_.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Opcion;
+            string Entrada;
+
+            do
+            {
+
+
+
+
+                Console.WriteLine("Universidad Ecci");
+                Console.WriteLine("Soluciones .Net Guia 1");
+                Console.WriteLine("Señor usuario digite la solucion a ejecutar");
+
+                Console.WriteLine("1. Operaciones Matematicas");
+                Console.WriteLine("2. Funciones Matematicas");
+                Console.WriteLine("3. Universidad Ecci");
+                Console.WriteLine("4. Soluciones .Net Guia 1");
+                Console.WriteLine("5. Operaciones Matematicas");
+                Console.WriteLine("6. Funciones Matematicas");
+                Console.WriteLine("7. Soluciones .Net Guia 1");
+                Console.WriteLine("8. Universidad Ecci");
+                Console.WriteLine("9. Soluciones .Net Guia 1");
+                Console.WriteLine("10. Operaciones Matematicas");
+
+
+
+
+                Entrada = Console.ReadLine();
+                Opcion = int.Parse(Entrada);
+
+                switch (Opcion)
+                {
+
+                    case 1:
+                        Ejercicio1 Ej1 = new Ejercicio1();
+                        Ej1.Programas();
+                        break;
+
+                    case 2:
+                        break;
+
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+
+                    case 5:
+                        break;
+
+                    case 6:
+                        break;
+                    case 7:
+                        break;
+
+                    case 8:
+                        break;
+
+                    case 9:
+                        break;
+
+                    case 10:
+
+                        break;
+
+    
+                    default: Console.WriteLine("La opcion no se encuentra en el menú");
+
+                }
+
+                Console.WriteLine("¿Desea Realizar Otra Operacion? Digite Si o No");
+                Entrada = Console.ReadLine();
+
+            } while (Entrada == "Si");
+
+
+
+
+        }
+    }
+}
+
