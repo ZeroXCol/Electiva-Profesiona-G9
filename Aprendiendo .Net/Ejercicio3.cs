5159c784e241dfaa6afac392de186cb16856e430
Fabian Martinez <xeroxcolombia15@gmail.com>
2021-03-12 19:59:57 -0500

    Adicion Punto 3 Corregido

--- Aprendiendo .Net/Ejercicio3.cs
+++ Aprendiendo .Net/Ejercicio3.cs
@@ -16,20 +16,21 @@ namespace Aprendiendo_.Net
             string cor = "";
             for (int i = 0; i < postulante; i++)
             {
-                for (int j = 0; j < 3; j++)
-                {
+                int user = 1;
+                
+                
                     cor = "Correcto";
-                    Console.WriteLine("Por favor La cantidad de respuestas " + cor + " del usuario " + i);
+                    Console.WriteLine("Por favor La cantidad de respuestas " + cor + " del usuario " + user);
                     val1 = int.Parse(Console.ReadLine());
                     val1 = val1 * 4;
 
                     cor = "Incorrecto";
-                    Console.WriteLine("Por favor La cantidad de respuestas " + cor + " del usuario " + i);
+                    Console.WriteLine("Por favor La cantidad de respuestas " + cor + " del usuario " + user);
                     val2 = int.Parse(Console.ReadLine());
                     val2 = val2 * (-1);
 
                     cor = "En blanco";
-                    Console.WriteLine("Por favor La cantidad de respuestas " + cor + " del usuario " + i);
+                    Console.WriteLine("Por favor La cantidad de respuestas " + cor + " del usuario " + user);
                     val3 = int.Parse(Console.ReadLine());
                     val3 = val3 * 0;
 
@@ -41,7 +42,8 @@ namespace Aprendiendo_.Net
                     val1 = 0;
                     val2 = 0;
                     val3 = 0;
-                }
+                    user++;
+                
 
             }
             Console.ReadLine();
